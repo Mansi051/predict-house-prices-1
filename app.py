bb26e422e146230458e54a1ba2758a4e177a9616
Mansi051 <mansiarora1701@gmail.com>
2025-12-18 00:22:09 +0530

    add city

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+st.set_page_config(
+    page_title="House Price Prediction",
+    page_icon="🏠",
+    layout="centered"
+)
+
+
 X=np.array([
     [800,1,20],
     [1000,1,15],
@@ -11,17 +18,34 @@ X=np.array([
     [2000,3,2]
 ])
 
-y=np.array([40,50,65,80,95,110])
+y=np.array([180,220,260,310,360,420])
 
 model=LinearRegression()
 model.fit(X,y)
 
-st.title("House Price Prediction App")
-size=st.number_input("House Size (sqft)", min_value=500, max_value=5000, value=1000)
-floors=st.number_input("Number of floors", min_value=1, max_value=5, value=1)
-age=st.number_input("Age of House(years)",min_value=0, max_value=50,value=5)
+st.title("🏠 House Price Prediction App")
+
+st.info(
+    """
+    This model is trained on **synthetic but realistic residential house price data
+    from suburban Mumbai, India**.
+
+    - Prices are in **Lakhs INR**
+    - Reflect typical Mumbai apartment pricing trends
+    - Intended for **educational demonstration only**
+"""
+)
+
+st.divider()
+st.subheader("Enter House Details")
+
+
+size=st.slider("House Size (sqft)", min_value=500, max_value=5000, value=1000, step=50)
+floors=st.selectbox("Number of floors", options=[1,2,3,4,5])
+age=st.slider("Age of House(years)",min_value=0, max_value=50,value=5)
 
 if st.button("Predict Prize"):
     input_data=np.array([[size,floors,age]])
     prediction=model.predict(input_data)
-    st.success(f"Estimated Price: Rs {prediction[0]:.2f} Lakhs")
+    st.success(f"""
+               **Estimated Price in Mumbai: Rs {prediction[0]:.2f} Lakhs**""")
